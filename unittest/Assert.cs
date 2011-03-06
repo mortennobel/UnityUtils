24ad93ecfe453420b0bc2ea0b834220bf6f3bfdd
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2011-03-06 10:13:52 +0100

    Add license header

--- unittest/Assert.cs
+++ unittest/Assert.cs
@@ -1,6 +1,11 @@
+/// UnityUtils https://github.com/mortennobel/UnityUtils
+/// By Morten Nobel-Jørgensen
+/// License lgpl 3.0 (http://www.gnu.org/licenses/lgpl-3.0.txt)
+
+
 /// <summary>
 /// subclass only to make naming simple
-/// </summary> 
+/// </summary>
 public class Assert : UUnitAssert {
 
 }
--- unittest/UUnitAssert.cs
+++ unittest/UUnitAssert.cs
@@ -1,3 +1,7 @@
+/// UnityUtils https://github.com/mortennobel/UnityUtils
+/// By Morten Nobel-Jørgensen
+/// License lgpl 3.0 (http://www.gnu.org/licenses/lgpl-3.0.txt)
+
 using UnityEngine;
 using System.Collections;
 using System;
