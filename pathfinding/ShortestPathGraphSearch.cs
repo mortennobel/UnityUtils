5b36b910bb60f889594db0901c14d4610c88909f
Morten Nobel-Joergensen <morten@nobel-joergensen.com>
2011-03-06 10:14:44 +0100

    Add license header

--- pathfinding/ShortestPathGraphSearch.cs
+++ pathfinding/ShortestPathGraphSearch.cs
@@ -1,3 +1,8 @@
+/// UnityUtils https://github.com/mortennobel/UnityUtils
+/// By Morten Nobel-Jørgensen
+/// License lgpl 3.0 (http://www.gnu.org/licenses/lgpl-3.0.txt)
+
+
 using UnityEngine;
 using System;
 using System.Collections.Generic;
